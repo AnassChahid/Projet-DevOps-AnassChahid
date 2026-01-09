c7b064fd5ed486a2df40b2961c29ca28c82f5ae2
AnassChahid <chahidanass77@gmail.com>
2026-01-09 16:16:18 +0100

    push in dev

--- application/src/main/java/com/example/demo/Application.java
+++ application/src/main/java/com/example/demo/Application.java
@@ -8,6 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
        
-        System.out.println("Bonjour et bon courage dans votre projet en DevOps ");
+        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
     }
 }
